d671449ad2d1bee226e3e3a1cdc0a8313a70bf5a
pleblira <91032628+pleblira@users.noreply.github.com>
2023-07-17 17:02:27 -0400

    buf ix

--- main.py
+++ main.py
@@ -55,15 +55,8 @@ def query_nostr_relays(type_of_query, query_term, since=0):
   message = json.dumps(request)
   relay_manager.publish_message(message)
   time.sleep(20) # allow the messages to send
-
+  
   relay_manager.close_connections()
-
-  # while relay_manager.message_pool.has_events():
-  #   event_msg = relay_manager.message_pool.get_event()
-  #   print("\n\n___________NEW_EVENT__________")
-  #   print(f"event.json: {event_msg.event.json}")
-  # quit()  
-
   return relay_manager.message_pool
 
 def seenOnNostr(start_time_for_first_run = 0):
@@ -132,13 +125,11 @@ def seenOnNostr(start_time_for_first_run = 0):
     # upon confirming it's a new event, checking if it's a reply or a new note
       non_mention_e_tags = []
       for tag in event_msg.event.json[2]["tags"]:
-        if "e" in tag:
-          for marker in tag:
-            if "mention" not in marker:
-              non_mention_e_tags.append(tag)
+        if "e" in tag and "mention" not in tag:
+          non_mention_e_tags.append(tag)
       if len(non_mention_e_tags) == 1:
-        print("single non_mention tag, means it's the reply")
-        query_term = non_mention_e_tags[1]
+        print("single non_mention tag, means it's the note to be retrieved")
+        query_term = non_mention_e_tags[0][1]
       else:
         # more than 1 non_mention tag, verifying which one is the reply and which one is the root
         for tag in non_mention_e_tags:
